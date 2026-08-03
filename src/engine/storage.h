e6af1b6e68bc6bcb984466e557d4a7cb620a92c5
Jaden Mu <68563142+NinjadenMu@users.noreply.github.com>
2026-08-03 01:21:18 -0400

    add storage in .bss for king info by ply

--- src/engine/storage.h
+++ src/engine/storage.h
@@ -8,6 +8,7 @@
 #define MINTI_ENGINE_STORAGE_H
 
 #include "../config.h"
+#include "attack.h"
 #include "types.h"
 
 // Arena for storing move lists
@@ -15,5 +16,11 @@ extern move_t move_arena[MOVE_ARENA_CAPACITY];
 
 // Pointers to start of each ply's move list in `move_arena`
 extern move_t *move_list_base[MAX_PLY + 2];
+
+/*
+ * king info is useful to compute since we can reuse it for legality checking 
+ * of every move in a ply.  Therefore, `king_info_t` structs are stored here.
+ */
+extern king_info_t king_info_stack[MAX_PLY + 2];
 
 #endif
