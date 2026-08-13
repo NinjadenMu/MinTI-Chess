1ec1e191de4cb9f047fff0247468ff21c6c584c0
Jaden Mu <68563142+NinjadenMu@users.noreply.github.com>
2026-08-13 12:45:41 -0400

    MinTI's data blend finalized, autotuning and testing almost done

--- src/host/main.c
+++ src/host/main.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 
 #include "../engine/engine.h"
+#include "../engine/position.h"
 #include "../engine/search.h"
 
 int main(void)
@@ -13,6 +14,8 @@ int main(void)
     return 1;
   }
 
+  position_from_fen("8/7p/8/2p3pP/3k2P1/P4K2/5P2/8 b - - 0 2");
+
   search_result_t result;
 
   if (search_position(4, &result) != 0) {
@@ -20,6 +23,7 @@ int main(void)
     return 1;
   }
 
+  printf("%hhu to %hhu\n", result.best_move.from, result.best_move.to);
   printf("%d\n", (int32_t)result.score);
   return 0;
 }
